7cbbbce10912c2fb82dae9d8f5b39d7bdacbdc6e
Ambud Sharma <ambudsharma@pinterest.com>
2020-11-18 14:03:25 -0800

    Initial implementation for yarn connector and nodes

--- orion-server/src/main/java/com/pinterest/orion/core/yarn/YarnCluster.java
+++ orion-server/src/main/java/com/pinterest/orion/core/yarn/YarnCluster.java
@@ -1,9 +1,14 @@
 package com.pinterest.orion.core.yarn;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import java.util.logging.Logger;
 
+import org.apache.hadoop.yarn.client.api.YarnClient;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.pinterest.orion.common.NodeInfo;
 import com.pinterest.orion.core.Cluster;
 import com.pinterest.orion.core.ClusterStateSink;
@@ -16,10 +21,16 @@ import com.pinterest.orion.core.actions.alert.AlertFactory;
 import com.pinterest.orion.core.actions.audit.ActionAuditor;
 import com.pinterest.orion.core.automation.operator.Operator;
 import com.pinterest.orion.core.automation.sensor.Sensor;
+import com.pinterest.orion.core.hbase.HBaseCluster;
 
 public class YarnCluster extends Cluster {
 
   private static final long serialVersionUID = 1L;
+  private static final Logger logger = Logger.getLogger(HBaseCluster.class.getName());
+  
+  private Map<String, Object> config;
+  @JsonIgnore
+  private transient YarnClient connection;
 
   public YarnCluster(String id,
                      String name,
@@ -31,39 +42,44 @@ public class YarnCluster extends Cluster {
                      ActionAuditor auditSink,
                      ClusterStateSink stateSink,
                      CostCalculator costCalculator) {
-    super(id, name, type, monitors, operators, actionFactory, alertFactory, auditSink, stateSink,
+    super(id, name, "YARN", monitors, operators, actionFactory, alertFactory, auditSink, stateSink,
         costCalculator);
-    // TODO Auto-generated constructor stub
   }
 
   @Override
   protected void bootstrapClusterInfo(Map<String, Object> config) throws PluginConfigurationException {
-    // TODO Auto-generated method stub
-    
+    this.config = config;
+    // core-site.xml
+    // yarn-site.xml
+    // hdfs-site.xml
   }
 
   @Override
   protected Node getNodeInstance(NodeInfo info) {
-    // TODO Auto-generated method stub
-    return null;
+    return new YarnNodeManager(this, info, new Properties());
   }
 
   @Override
   public void addNodeWithoutAgent(NodeInfo info) {
-    // TODO Auto-generated method stub
-    
+    getNodeMap().put(info.getNodeId(), new YarnNodeManager(this, info, new Properties()));
   }
 
   @Override
   public Logger logger() {
-    // TODO Auto-generated method stub
-    return null;
+    return logger;
   }
 
   @Override
   public Map<String, Utilization> getUtilizationMap() {
-    // TODO Auto-generated method stub
-    return null;
+    return new HashMap<>();
+  }
+
+  public YarnClient getConnection() {
+    return connection;
+  }
+
+  public void setConnection(YarnClient connection) {
+    this.connection = connection;
   }
 
 }
