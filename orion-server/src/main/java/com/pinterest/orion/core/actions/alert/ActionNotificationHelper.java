1deb6588856333aefe8658b4b23bdcc77a0dd1a9
yisheng-zhou <zhouys93@hotmail.com>
2022-09-30 15:29:25 -0700

    Implement ActionNotificationHelper

---
+++ orion-server/src/main/java/com/pinterest/orion/core/actions/alert/ActionNotificationHelper.java
@@ -0,0 +1,129 @@
+package com.pinterest.orion.core.actions.alert;
+
+import com.pinterest.orion.core.actions.Action;
+import org.glassfish.jersey.client.JerseyClientBuilder;
+
+import javax.ws.rs.client.WebTarget;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ActionNotificationHelper {
+
+    public static final String ATTR_SLACK_WEBHOOK_URL_LIST_KEY = "slackWebhookUrlList";
+    private Action action;
+    private String alertTitle;
+    private String alertMessage;
+    private boolean isSendingSlackNotification = false;
+    private List<String> slackWebhookUrlList;
+    private Logger logger;
+
+    public ActionNotificationHelper(Action action, Map<String, Object> config) {
+        // Use action and its config to construct ActionNotificationHelper.
+        setAction(action);
+        setAlertTitle(action.getName());
+        setAlertMessage(action.getName());
+        setLogger(action.logger());
+        // Config determines which types of notification will be sent. Can add more types of notifications here.
+        if (config.containsKey(ATTR_SLACK_WEBHOOK_URL_LIST_KEY)) {
+            Object slackWebhookUrlListObject = config.get(ATTR_SLACK_WEBHOOK_URL_LIST_KEY);
+            if (slackWebhookUrlListObject != null) {
+                if (slackWebhookUrlListObject instanceof List) {
+                    this.isSendingSlackNotification = true;
+                    this.slackWebhookUrlList = (List<String>) config.get(ATTR_SLACK_WEBHOOK_URL_LIST_KEY);
+                } else {
+                    // Log warn if slackWebhookUrlList cannot be parsed from config.
+                    getLogger().log(Level.WARNING, ATTR_SLACK_WEBHOOK_URL_LIST_KEY + " value is unacceptable type.");
+                }
+            } else {
+                getLogger().log(Level.WARNING, ATTR_SLACK_WEBHOOK_URL_LIST_KEY + " value is null.");
+            }
+        }
+    }
+
+    public void setAction(Action action) {
+        this.action = action;
+    }
+
+    public void setAlertTitle(String alertTitle) {
+        // Call this in Action class if you need personalized alert title.
+        this.alertTitle = alertTitle;
+    }
+
+    public void setAlertMessage(String alertMessage) {
+        // Call this in Action class if you need personalized alert message.
+        this.alertMessage = alertMessage;
+    }
+
+    public void setLogger(Logger logger) {
+        // Use logger from action. If action does not have logger, create one.
+        if (logger != null) {
+            this.logger = logger;
+        } else {
+            this.logger = Logger.getLogger(this.getClass().getName());
+        }
+    }
+
+    public Action getAction() {
+        return this.action;
+    }
+
+    public String getAlertTitle(){
+        return this.alertTitle;
+    }
+
+    public String getAlertMessage() {
+        return this.alertMessage;
+    }
+
+    public Logger getLogger() {
+        return this.logger;
+    }
+
+    public List<String> getSlackWebhookUrlList() {
+        return this.slackWebhookUrlList;
+    }
+
+    protected static List<WebTarget> getWebTargetsFromWebhookUrlList(List<String> webhookUrlList) {
+        // Transfer webhook urls (String) to web targets (WebTarget)
+        List<WebTarget> webTargets = new ArrayList<>();
+        for (int i = 0; i < webhookUrlList.size(); i++) {
+            WebTarget webTarget = JerseyClientBuilder.newClient().target(webhookUrlList.get(i));
+            webTargets.add(webTarget);
+        }
+        return webTargets;
+    }
+
+    private void sendSlackNotifications() {
+        SlackAlert slackAlert = new SlackAlert();
+        try {
+            List<WebTarget> webTargets = getWebTargetsFromWebhookUrlList(getSlackWebhookUrlList());
+            slackAlert.setWebTargets(webTargets);
+        } catch (Exception e) {
+            getLogger().log(Level.WARNING, "ActionNotificationHelper fails to get web targets: " + e);
+            return;
+        }
+        AlertMessage slackAlertMessage = new AlertMessage(
+                getAlertTitle(),
+                getAlertMessage(),
+                getAction().getOwner()
+        );
+        try {
+            getAction().getEngine().alert(slackAlert, slackAlertMessage);
+            getLogger().log(Level.INFO, "ActionNotificationHelper has sent slack notifications.");
+        } catch (Exception e) {
+            getLogger().log(Level.WARNING, "ActionNotificationHelper fails to send Slack message: " + e);
+        }
+    }
+
+    public void sendNotification() {
+        if (isSendingSlackNotification) {
+            sendSlackNotifications();
+        } else {
+            getAction().logger().log(
+                    Level.INFO, "ActionNotificationHelper does not send any notification.");
+        }
+    }
+}
---
+++ orion-server/src/test/java/com/pinterest/orion/core/actions/alert/ActionNotificationHelperTest.java
@@ -0,0 +1,76 @@
+package com.pinterest.orion.core.actions.alert;
+
+import com.pinterest.orion.core.actions.Action;
+import com.pinterest.orion.core.actions.ActionEngine;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+public class ActionNotificationHelperTest {
+
+    @Test
+    public void testNoNotification() throws Exception {
+        Action action = Mockito.mock(Action.class);
+        Logger logger = Mockito.mock(Logger.class);
+        ActionEngine engine =  Mockito.mock(ActionEngine.class);
+        // Mock action
+        Mockito.when(action.logger()).thenReturn(logger);
+        Mockito.when(action.getEngine()).thenReturn(engine);
+        // Create a config
+        Map<String, Object> emptyConfig = new HashMap<>();
+        // Create notification helper from mock action and config
+        ActionNotificationHelper notificationHelper = new ActionNotificationHelper(
+                action, emptyConfig
+        );
+        // Validate sendNotification method
+        notificationHelper.sendNotification();
+        verify(action, times(0)).getEngine();
+    }
+
+    @Test
+    public void testSendSlackNotification() throws Exception {
+        Action action = Mockito.mock(Action.class);
+        Logger logger = Mockito.mock(Logger.class);
+        ActionEngine engine =  Mockito.mock(ActionEngine.class);
+        // Mock action
+        Mockito.when(action.logger()).thenReturn(logger);
+        Mockito.when(action.getEngine()).thenReturn(engine);
+        Mockito.when(action.getOwner()).thenReturn("test_owner");
+        // Create a config
+        List<String> testWebhookUrlList = new ArrayList<String>() {{
+            add("test_webhook_url");
+        }};
+        Map<String, Object> config = new HashMap<String, Object>(){{
+            put(ActionNotificationHelper.ATTR_SLACK_WEBHOOK_URL_LIST_KEY, testWebhookUrlList);
+        }};
+        // Create notification helper from mock action and config
+        ActionNotificationHelper notificationHelper = new ActionNotificationHelper(
+                action, config
+        );
+        // Test parameters
+        notificationHelper.setAlertTitle("test_title");
+        notificationHelper.setAlertMessage("test_message");
+        assertEquals(testWebhookUrlList, notificationHelper.getSlackWebhookUrlList());
+        // Create alerts and alert message
+        AlertMessage testSlackAlertMessage = new AlertMessage(
+                "test_title",
+                "test_message",
+                "test_owner"
+        );
+        SlackAlert testSlackAlert = new SlackAlert();
+        testSlackAlert.setWebTargets(ActionNotificationHelper.getWebTargetsFromWebhookUrlList(testWebhookUrlList));
+        // Validate sendNotification method
+        notificationHelper.sendNotification();
+        verify(action, times(1)).getEngine();
+        verify(engine, times(1)).alert(testSlackAlert, testSlackAlertMessage);
+    }
+}
